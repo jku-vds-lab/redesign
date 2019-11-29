e44eb4a7d7231ca891b7932fd52514b3e08153c6
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-11-29 12:22:46 +0100

    Import style in typescript instead of html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+import './style.scss'; // import styles as described https://github.com/webpack-contrib/sass-loader
 
 document.title = 'Redesign';
-document.getElementById('heading').textContent = 'Hello Worlds!'
+document.getElementById('heading').textContent = 'Hello World!'
 console.log('test')
