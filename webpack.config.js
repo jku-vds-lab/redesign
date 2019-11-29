8c58ff1611f236bf941d4b5a8eaea2230939f584
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-11-29 12:21:16 +0100

    Add sass rules to webpack confug

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,22 @@ module.exports = {
   devtool: 'inline-source-map',
   module: {
     rules: [
-      {
+      { // typescript files
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
+      }, 
+      { // sass files
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
     ],
   },
   resolve: {
