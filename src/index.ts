26e317b4d16986605b0bdc256042eb157bd6a9c3
Oleg Lesota <cheesemaid@gmail.com>
2019-12-10 10:17:18 +0100

    draco editor, constrains list

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,46 @@ import Draco from 'draco-vis';
 import embed from 'vega-embed';
 
 document.title = 'Redesign';
-document.getElementById('heading').textContent = 'Hello World!'
-console.log('test')
+document.getElementById('heading').textContent = 'Draco Testbench';
+document.getElementById("draco_query").value = ` % ====== Data definitions ======
+data("parties.json").
+num_rows(6).
+
+fieldtype("count",number).
+cardinality("count",6).
+
+fieldtype("party",string).
+cardinality("party",6).
+
+% ====== Query constraints ======
+encoding(e0).
+:- not field(e0,"count").
+
+encoding(e1).
+:- not field(e1,"party").`;
+console.log('test0')
 
 const runDraco = async () => {
-  //Copied from observable:
+  console.log('RunDraco')
   // initialize draco with the clingo webassembly module
   const draco = await (new Draco()).init()
-  const result = draco.solve(`
-    % ====== Data definitions ======
-    data("https://vega.github.io/editor/data/cars.json").
-    num_rows(142).
-    
-    fieldtype("Horsepower",number).
-    cardinality("Horsepower",94).
-    
-    fieldtype("Acceleration",number).
-    cardinality("Acceleration",96).
-    
-    % ====== Query constraints ======
-    encoding(e0).
-    :- not field(e0,"Acceleration").
-    
-    encoding(e1).
-    :- not field(e1,"Horsepower").
-  `)
-  console.log(result)
+  console.log(draco);
+ // console.log(">>>",document.getElementById("draco_query").textContent);
+  const result = draco.solve(document.getElementById("draco_query").value);
+  console.log("SOLVED!>",result);
+  let softCons = "";
+  for (let i=0; i<(result.models[0].violations).length; i++){
+      let curViolation = result.models[0].violations[i];
+      softCons += curViolation.description + " weight: " + curViolation.weight + "<br>";
+  }
+  console.log(softCons);
+  document.getElementById('soft_con').innerHTML = softCons;
+  embed('#vega',result.specs[0]);
 }
 
- runDraco();
-
-embed('#vega', {
+//runDraco();
+document.getElementById('draco_launch').addEventListener("click", runDraco);
+/*embed('#vega', {
   "$schema": "https://vega.github.io/schema/vega-lite/v4.0.0-beta.12.json",
   "description": "A scatterplot showing horsepower and miles per gallons for various cars.",
   "data": {"url": "https://vega.github.io/editor/data/cars.json"},
@@ -43,3 +52,22 @@ embed('#vega', {
     "y": {"field": "Miles_per_Gallon", "type": "quantitative"}
   }
 });
+
+`
+ % ====== Data definitions ======
+ data("parties.json").
+ num_rows(6).
+ 
+ fieldtype("count",number).
+ cardinality("count",6).
+ 
+ fieldtype("party",string).
+ cardinality("party",6).
+ 
+ % ====== Query constraints ======
+ encoding(e0).
+ :- not field(e0,"count").
+ 
+ encoding(e1).
+ :- not field(e1,"party").
+`*/
