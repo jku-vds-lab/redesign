36023f3227c78c76ba2bc17a7c8e2a55bea3e904
Oleg Lesota <oleglesota@gmail.com>
2020-01-26 13:53:43 +0100

    deleted commented code

--- src/index.ts
+++ src/index.ts
@@ -42,27 +42,7 @@ const reason_plot = () => {
 document.getElementById('draco_reason').addEventListener("click", reason_plot);
 init_draco();
 console.log('this is brushed')
-/*
-const runDraco = async () => {
-  console.log('RunDraco')
-  // initialize draco with the clingo webassembly module
-  const draco = await (new Draco()).init()
-  console.log(draco);
- // console.log(">>>",document.getElementById("draco_query").textContent);
-  const result = draco.solve(dracoQuery.value);
-  console.log("SOLVED!>",result);
-  let softCons = "";
-  for (let i=0; i<(result.models[0].violations).length; i++){
-      let curViolation = result.models[0].violations[i];
-      softCons += curViolation.description + " weight: " + curViolation.weight + "<br>";
-  }
-  console.log(softCons);
-  document.getElementById('soft_con').innerHTML = softCons;
-  embed('#vega',result.specs[0]);
-}
 
-//runDraco();
-document.getElementById('draco_launch').addEventListener("click", runDraco);
 /*embed('#vega', {
   "$schema": "https://vega.github.io/schema/vega-lite/v4.0.0-beta.12.json",
   "description": "A scatterplot showing horsepower and miles per gallons for various cars.",
@@ -73,22 +53,4 @@ document.getElementById('draco_launch').addEventListener("click", runDraco);
     "y": {"field": "Miles_per_Gallon", "type": "quantitative"}
   }
 });
-
-`
- % ====== Data definitions ======
- data("parties.json").
- num_rows(6).
- 
- fieldtype("count",number).
- cardinality("count",6).
- 
- fieldtype("party",string).
- cardinality("party",6).
- 
- % ====== Query constraints ======
- encoding(e0).
- :- not field(e0,"count").
- 
- encoding(e1).
- :- not field(e1,"party").
-`*/
+*/
