15aa9d0485535e11b98b44ce463586e7751d204f
Oleg Lesota <oleglesota@gmail.com>
2020-04-29 16:17:57 +0200

    removed old code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import './style.scss'; // import styles as described https://github.com/webpack-contrib/sass-loader
 import Draco from 'draco-vis';
 import embed from 'vega-embed';
-import {costsDict} from './costsDictionary';
-import * as tsnejs from './tsne';
-import * as d3 from 'd3';
 
 document.title = 'Graph Destroyer';
 document.getElementById('heading').textContent = 'Graph Destroyer';
@@ -16,10 +13,7 @@ let dataSummary: any;
 let dataSetIndex = 0;
 
 let currentEncodings = {};
-let currentViolationsSummary: {};
 let currentResult: any;
-
-let currentVectors = [];
 
 const arrSum = arr => arr.reduce((a,b) => a + b, 0);
 
@@ -153,247 +147,21 @@ const generateDRACOSpecification = () => {
 
 const reason_plot = () => {
   let spec = generateDRACOSpecification();
-  //const result = draco_instance.solve((document.getElementById("draco_query") as HTMLTextAreaElement).value);
-  //let number_of_models = (document.getElementById('n_models')as HTMLInputElement).value as unknown as number;
   let number_of_models = 1;
   currentResult = draco_instance.solve(spec,{"models":number_of_models});
   console.log("Specification solved: ",currentResult);
-  
-  //formatRules(currentResult);
-  //let sortedViolations = displayViolations(false);
-  //document.getElementById('soft_con').innerHTML = softCons;
-  //document.getElementById('soft_con').innerHTML = sortedViolations;
-  //updateLeft();
-  //updateRight();
-  (document.getElementById("vega_spec")as HTMLInputElement).value = JSON.stringify(currentResult.specs[0]);
- // (document.getElementById("index_right")as HTMLInputElement).max = (number_of_models - 1) as unknown as string ;
-  /* exporting visual Embeddings */
-  exportVisualEmbeddings();
+
+  (document.getElementById("vega_spec")as HTMLInputElement).value = JSON.stringify(currentResult.specs[0]).replace(/,\"/g,",\n\"");
+  updatePlot();
 }
 /* VEGA */
-const updateLeft = () => {
-  let index = (document.getElementById("index_left")as HTMLInputElement).value as unknown as number;
-  embed('#vega_left',currentResult.specs [index]);
-  document.getElementById('vis_left_header').innerHTML = "Vis #"+ index as string + "; Cost: " + currentResult.models[index].costs as string;
-}
-const updateRight = () => {
+const updatePlot = () => {
   let spec = JSON.parse((document.getElementById("vega_spec")as HTMLInputElement).value);
   embed('#vega_right',spec);
 }
 
-/* 
-Gather Violations
-*/
-const formatRules = (dracoOut :  any) => {
-  let sortedViolatons = {};
-  let enc_list = Object.keys(currentEncodings);
-  enc_list.push("other");
-  currentEncodings["other"] = {"type":"unknown", "name":"other"};
-  for (let i=0; i<(enc_list).length; i++){
-    sortedViolatons[enc_list[i]] = new Array;
-  }
-  for (let i=0; i<(dracoOut.models[0].violations).length; i++){
-    let curViolation = dracoOut.models[0].violations[i];
-    let curEncoding = ((curViolation.witness as string).match(/,(.*)\)/)[1] as string);
-    if (!enc_list.includes(curEncoding)) {
-      curEncoding = "other";
-    }
-    sortedViolatons[curEncoding].push({ desc: (curViolation.description as string),
-                                        weight: (curViolation.weight as number)});
-  }
-  currentViolationsSummary = sortedViolatons;
-  return(sortedViolatons);
-}
-/*
-Display Violations
-*/
-const displayViolations = (zeros: boolean = true) => {
-let headers = Object.keys(currentViolationsSummary);
-let resHTML = "";
-for(let i=0; i<headers.length ; i++){
-  let curEncoding = headers[i] as string;
-  let violations = currentViolationsSummary[curEncoding];
-  if (violations.length < 1) continue;
-  resHTML += "<h3>" + "field " + currentEncodings[curEncoding]["name"] + ":</h3><ul>"
-  for (let j=0; j<violations.length; j++){
-    if ((violations[j].weight == 0) && !zeros) continue;
-    resHTML += "<li> " + violations[j]["desc"] + "[" + violations[j].weight +"];</li>";
-  }
-  resHTML += "</ul>";
-}
-return resHTML;
-}
 
-const exportVisualEmbeddings = () => {
-  currentVectors = [];
-  const numberOfSelectedFields = Object.keys(currentEncodings).length - 1; // (-1 because of default <other> encoding)
-  const spaceCardinality = Object.keys(costsDict).length * (numberOfSelectedFields);
-  const enc_list = Object.keys(currentEncodings);
-  const violationsArray = Object.keys(costsDict);
-  console.log("Vector Space Cardinality (#soft constraints times #number of fields): ",spaceCardinality);
-  console.log("Number of fields selected: ", numberOfSelectedFields, enc_list);
-  /*
-  === Clarification! ===
-  currentEncodings - contains mapping from data fields to encodings (entities representing data on screen)
-  encodings are e0, e1, ... how many? exactly how many fields have been selected;
-  there is also a special "encoding" <other> which is an artificial entity for easier sorting of violations
-  (for violations which do not relate to an encoding, but to the whole plot);
-  we do not consider <other> for building a feature vector. In our vector we have one element for each
-  combintaion: {violation_i + encoding_j};
-  this means that if we have 2 encodings (or fields selected) for violation with number k,
-  that is applied to the whole plot, we will have 2 identical values in our vector, one for each encoding;
-  */  
-  if (currentResult.models.length > 1000) {
-    alert("Prevented attempted to produce more than 1000 vector embeddings. Remove this restriction in the code.");
-    return;
-  }
-  currentResult.models.forEach((model: { violations: any[]; }) => {
-    let curVector = new Array(spaceCardinality).fill(0);
-    model.violations.forEach(violation => {
-      // figure violation index
-      const violationIndex = violationsArray.lastIndexOf(violation["name"]+"_weight");
-      // figure encoding index
-      let curEncoding = ((violation.witness as string).match(/,(.*)\)/)[1] as string);
-      let encodingIndices = [];
-      if (!enc_list.includes(curEncoding)) {
-        for(let i = 0; i < numberOfSelectedFields; i++){
-          encodingIndices.push(i);
-        }
-      }
-      else {
-        encodingIndices.push(+curEncoding.substring(1));
-      }
-      // figure cost of violation
-      const cost = +costsDict[violation.name + "_weight"];
-      encodingIndices.forEach(index => {
-        curVector[index * (Object.keys(costsDict).length) + violationIndex] = cost;
-      })
-    });
-    currentVectors.push(curVector);
-    //console.log(curVector);
-    //console.log(arrSum(curVector));
-  });
-  console.log(currentVectors);
-  // T-SNE
-  const opt = {
-    "epsilon" : 10, // epsilon is learning rate (10 = default)
-    "perplexity" : 30, // roughly how many neighbors each point influences (30 = default)
-    "dim" : 2 // dimensionality of the embedding (2 = default)
-  };
-
-  let tsne = new tsnejs.tSNE(opt); // create a tSNE instance
-  tsne.initDataRaw(currentVectors);
-  for(let k = 0; k < 500; k++) {//!!!!
-    tsne.step(); // every time you call this, solution gets better
-  }
-  //
-  //
-  //
-  //
-  ///
-//
-  //
-  const Y0 = tsne.getSolution();
-  //Y = [[1,1], [2,5], [4,3], [1.5,1.5]];
-  ///
-
-  ///
-  //
-  //
-  //
-  //
-  console.log(Y0);
-  //ADDING COSTS TO T-SNE VECTOR OUTPUT
-  let Y = [];
-  for (let i=0; i<Y0.length; i++){
-    Y.push({"x":Y0[i][0],"y":Y0[i][1],"cost": currentResult.models[i].costs[0],"index":i});}
-  console.log(Y);
-
-
-  // plotting
-  const margin = {top: 30, right: 30, bottom: 30, left: 30};
-  const width = 250 - margin.left - margin.right;
-  const height = 250 - margin.top - margin.bottom;
-
-  d3.select("#t_sne_plot").select("svg").remove();
-  let svg = d3.select("#t_sne_plot")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// setup x 
-    const maxX = d3.max(Y, function(d) { return +d.x;} );
-    const minX = d3.min(Y, function(d) { return +d.x;} );
-    let xScale = d3.scaleLinear()
-      .domain([minX,maxX])
-      .range([0,width]);
-    let xAxis = d3.axisTop(xScale);
-    const xMap = function(d) { return xScale(d.x);}
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .call(xAxis)
-// setup y
-
-    const maxY = d3.max(Y, function(d) { return +d.y;} );
-    const minY = d3.min(Y, function(d) { return +d.y;} );
-    let yScale = d3.scaleLinear()
-      .domain([minY,maxY])
-      .range([0,height]);
-    let yAxis = d3.axisLeft(yScale);
-    const yMap = function(d) { return yScale(d.y);}
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-      let tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0);
-
-  svg.selectAll("circle")
-  .data(Y)
-  .enter()
-  .append("circle")
-  .attr("cx", xMap)
-  .attr("cy", yMap)
-  .attr("r", 4)
-  .attr("fill", "steelblue")
-  .on("mouseover", function(d) {
-    tooltip.transition()
-         .duration(200)
-         .style("opacity", .9);
-    tooltip.html("Cost: "+d.cost+"<br>Index: "+d.index)
-         .style("left", (d3.event.pageX + 5) + "px")
-         .style("top", (d3.event.pageY - 28) + "px");})
-  .on("mouseout", function(d) {
-    tooltip.transition()
-         .duration(500)
-         .style("opacity", 0);})
-  .on("click", function(d){
-    (document.getElementById("index_left")as HTMLInputElement).value = d.index as unknown as string;
-    updateLeft();
-  });
-//*/
-console.log("X:", minX, maxX,"\nY:",minY, maxY);
-}   
 
 document.getElementById('draco_reason').addEventListener("click", reason_plot);
-document.getElementById('build_graph').addEventListener("click", updateRight);
-//document.getElementById('index_left').addEventListener("change", updateLeft);
-//document.getElementById('index_right').addEventListener("change", updateRight);
-
+document.getElementById('build_graph').addEventListener("click", updatePlot);
 init_draco();
-
-/*embed('#vega', {
-  "$schema": "https://vega.github.io/schema/vega-lite/v4.0.0-beta.12.json",
-  "description": "A scatterplot showing horsepower and miles per gallons for various cars.",
-  "data": {"url": "https://vega.github.io/editor/data/cars.json"},
-  "mark": "point",
-  "encoding": {
-    "x": {"field": "Horsepower", "type": "quantitative"},
-    "y": {"field": "Miles_per_Gallon", "type": "quantitative"}
-  }
-});
-*/
